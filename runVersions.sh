171e92b0d4acc04f89e1ce024f090d0613242474
apeltzer <alex.peltzer@mailbox.org>
2017-07-19 15:44:16 +0200

    Updated versions

--- runVersions.sh
+++ runVersions.sh
@@ -3,17 +3,29 @@
 
 #Getting them systemwide without archlinux support
 #EAGER-GUI and CLI already do that with the logfile anyways
+echo "GATK Version:"
 gatk --version
+echo "Java Version:"
 java -version
+echo "DeDup Version:"
 dedup -h | grep "DeDup v."
+AdapterRemoval --version
+echo "ClipAndMerge Version:"
 ClipAndMerge 2>&1 | grep -e "ClipAndMerge (v."
+echo "FastQC Version:"
 fastqc -version
+echo "Preseq Version:"
 preseq -version
+echo "VCF2Genome Version:"
 vcf2genome 2>&1 | grep -e "(v. "
+echo "BWA Version:"
 bwa 2>&1 | grep -e "Version" -e "Program:"
-echo "mapDamage: "
+echo "mapDamage Version: "
 mapDamage --version
+echo "GNU R Version:"
 R --version | grep "R version"
-ReportTable
+#ReportTable needs fix
+echo "Bowtie2 Version:"
 bowtie2 --version|grep -e "version"
-angsd | grep -e "version:" -m 1
+echo "ANGSD Version:"
+angsd  2>&1| grep -e "version:" -m 1
