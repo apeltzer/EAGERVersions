af1bbf733dcb54cfc1924c263cb806b5f2e8501d
peltzer <alexander.peltzer@uni-tuebingen.de>
2017-07-19 15:51:14 +0200

    Fix mapDamage

--- runVersions.sh
+++ runVersions.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #This script determines the installed versions of each software tool within the EAGER pipeline and tells the user about it. 
 
-#pacman -Q eager-gui eager-cli gatk jdk dedup circularmapper clipandmerge fastqc preseq vcf2genome bwa mapdamage fastx_toolkit htslib eagerstat r eager-reportengine bowtie2 picard-tools stampy angsd schmutzi
-
 #Getting them systemwide without archlinux support
 #EAGER-GUI and CLI already do that with the logfile anyways
 gatk --version
@@ -13,7 +11,8 @@ fastqc -version
 preseq -version
 vcf2genome 2>&1 | grep -e "(v. "
 bwa 2>&1 | grep -e "Version" -e "Program:"
-echo "mapDamage: " mapDamage --version
+echo "mapDamage: "
+mapDamage --version
 R --version | grep "R version"
 ReportTable
 bowtie2 --version|grep -e "version"
