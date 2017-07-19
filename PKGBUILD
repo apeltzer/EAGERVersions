6c572cbd769f61453ce23e0221b70528d66d161b
apeltzer <alex.peltzer@mailbox.org>
2017-07-19 16:10:50 +0200

    Updated files

---
+++ PKGBUILD
@@ -0,0 +1,17 @@
+# Contributor: Alex Peltzer <alex.peltzer@gmail.com>
+# Maintainer: Alex Peltzer <alex.peltzer@gmail.com>
+
+pkgname=eagerversions
+pkgver=1.92
+pkgrel=1
+pkgdesc="A small tool to generate versions of utilized tools for the logfile."
+arch=('any')
+url="http://it.inf.uni-tuebingen.de/?page_id=161"
+license=('custom')
+depends=('sh')
+source=("eager_versions.sh")
+md5sums=('SKIP')
+package() {
+  mkdir -p "${pkgdir}/usr/bin"
+  install -D -m755 "${srcdir}/eager_versions.sh" "${pkgdir}/usr/bin/eagerVersions"
+}
---
+++ eager_versions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#This script determines the installed versions of each software tool within the EAGER pipeline and tells the user about it. 
+
+#$1 = output file to write to
+#Getting them systemwide without archlinux support
+#EAGER-GUI and CLI already do that with the logfile anyways
+echo "GATK Version:" >> $1
+gatk --version 2>&1 >> $1 
+echo "Java Version:" >> $1 
+java -version 2>&1 >> $1
+echo "DeDup Version:" >> $1
+dedup -h | grep "DeDup v." 2>&1  >> $1
+echo "AdapterRemoval Version:" >> $1
+AdapterRemoval --version >> $1 
+echo "ClipAndMerge Version:" >> $1 
+ClipAndMerge 2>&1 | grep -e "ClipAndMerge (v." >> $1
+echo "CircularMapper Version:" >> $1
+realignsamfile -h | grep -e "usage" 2>&1  >> $1
+echo "FastQC version:" >> $1
+fastqc -version 2>&1 >> $1
+echo "Preseq Version:" >>$1
+preseq -version 2>&1| grep -e "Version: " >> $1
+echo "VCF2Genome Version:" >> $1
+vcf2genome 2>&1 | grep -e "(v. " >> $1
+echo "BWA Version:" >> $1
+bwa 2>&1 | grep -e "Version" >> $1
+echo "mapDamage Version: " >> $1
+mapDamage --version 2>&1 >> $1
+echo "GNU R Version:" >> $1
+R --version | grep "R version" 2>&1 >> $1
+#ReportTable needs fix
+echo "Bowtie2 Version:" >> $1
+bowtie2 --version|grep -e "s version" 2>&1  >> $1
+echo "ANGSD Version:" >> $1
+angsd  2>&1| grep -e "version:" -m 1 >> $1
